72636f34f6323d298e69c524be010c7c8bac06f1
KeMezz <88423849+KeMezz@users.noreply.github.com>
2022-02-14 13:01:12 +0900

    location 설정 추가

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 
-// const API_KEY = "ae1744e07c381f0dac0de7fb4e5a98fd";
+const API_KEY = "ae1744e07c381f0dac0de7fb4e5a98fd";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(false);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) setOk(false);
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    getWeather();
+  });
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.city}>
-          <Text style={styles.cityName}>Seoul</Text>
+          <Text style={styles.cityName}>{city}</Text>
         </View>
         <ScrollView
           horizontal
@@ -50,12 +72,12 @@ const styles = StyleSheet.create({
     backgroundColor: "gold",
   },
   city: {
-    flex: 1,
+    flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
   },
   cityName: {
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: "500",
   },
   weather: {},
@@ -65,7 +87,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 160,
-    marginTop: 150,
+    marginTop: 100,
   },
   desc: {
     marginTop: -10,
